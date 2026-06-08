17719f4f382a1d163433d2d585f5c274f13afa0e
Shahnawaz Shah <shahcreation6158@gmail.com>
2026-06-08 23:21:53 +0530

    added gsap styling to the hello page

--- personal-Portfolio/src/pages/Hello.jsx
+++ personal-Portfolio/src/pages/Hello.jsx
@@ -3,30 +3,30 @@ import { gsap } from 'gsap'
 import { useEffect,useRef } from 'react'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import { useGSAP } from '@gsap/react'
+import { h2 } from 'framer-motion/client'
 
 gsap.registerPlugin(ScrollTrigger)
 
 const Hello = () => {
   const gsapRef = useRef()
   useGSAP(()=>{
-    gsap.from(gsapRef.current,{
+    gsap.from(gsapRef.current.querySelectorAll('h2'),{
     y:20,opacity:0,stagger:0.2,
     scrollTrigger: {
-        trigger: "#hello",
+        trigger: gsapRef.current,
         scroller: "body",
-        markers: true,
         scrub: true,
-        start: "top 90%"}
+        start: "top 120%"}
 })
   })
   return (
     <div>
       <div ref={gsapRef} className='hellodiv flex bg-black text-yellow-300 min-h-screen justify-center items-center gap-3'>
-      <h2 ref={gsapRef} className='text-9xl gap-2'>H</h2>
-      <h2 className='text-9xl'>e</h2>
-      <h2 className='text-9xl'>l</h2>
-      <h2 className='text-9xl'>l</h2>
-      <h2 className='text-9xl'>o</h2>
+      <h2 className='text-[37em] gap-2'>H</h2>
+      <h2 className='text-[37em]'>e</h2>
+      <h2 className='text-[37em]'>l</h2>
+      <h2 className='text-[37em]'>l</h2>
+      <h2 className='text-[37em]'>o</h2>
       </div>
     </div>
   )
