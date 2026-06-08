0add9bae1aa78e84267960669daffb8da868203e
Shahnawaz Shah <shahcreation6158@gmail.com>
2026-06-08 22:22:53 +0530

    some more styling added

--- personal-Portfolio/src/pages/Hello.jsx
+++ personal-Portfolio/src/pages/Hello.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { gsap } from 'gsap'
+import { useEffect,useRef } from 'react'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hello = () => {
+  const gsapRef = useRef()
+  useGSAP(()=>{
+    gsap.from(gsapRef.current,{
+    y:20,opacity:0,stagger:0.2,
+    scrollTrigger: {
+        trigger: "#hello",
+        scroller: "body",
+        markers: true,
+        scrub: true,
+        start: "top 90%"}
+})
+  })
   return (
     <div>
-      <div className='hellodiv flex bg-black text-yellow-300 min-h-screen justify-center align-middle gap-3'>
-      <h2 className='text-9xl gap-2'>H</h2>
+      <div ref={gsapRef} className='hellodiv flex bg-black text-yellow-300 min-h-screen justify-center items-center gap-3'>
+      <h2 ref={gsapRef} className='text-9xl gap-2'>H</h2>
       <h2 className='text-9xl'>e</h2>
       <h2 className='text-9xl'>l</h2>
       <h2 className='text-9xl'>l</h2>
